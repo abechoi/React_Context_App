a8682960d093ac2ad2724755fca9cf0a6aea9ad7
abechoi <abraham.choi@icloud.com>
2020-07-09 20:30:52 -0700

    Creating Context with Functional Components

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -1,4 +1,5 @@
 import React, { createContext, Component } from "react";
+// import React, { useState } from "react";
 
 export const ThemeContext = createContext();
 
@@ -6,11 +7,13 @@ class ThemeContextProvider extends Component {
     state = { 
         isLightTheme: true,
         light: { syntax: '#555', ui: '#ddd', bg: '#eee' },
-        dark: { syntax: '#ddd', ui: '#333', bg: '#555'}
+        dark: { syntax: '#ddd', ui: '#333', bg: '#555' }
     }
+    // const [ theme, setTheme] = useState({ isLightTheme: true, light: { syntax: '#555', ui: '#ddd', bg: '#eee' }, dark: { syntax: '#ddd', ui: '#333', bg: '#555'} });
 
     toggleTheme = () => {
         this.setState({ isLightTheme: !this.state.isLightTheme });
+        // setTheme(state => ({ isLightTheme: !state.isLightTheme }));
     }
      
     render() { 
